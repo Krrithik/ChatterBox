db74737de962a86dc404c1650eba4b6b9b3f63ae
Krrithik <krrithikezhil30@gmail.com>
2025-05-08 11:49:05 -0700

    basic styling done

--- frontend/Chatterbox-frontend/src/components/ChatContainer.jsx
+++ frontend/Chatterbox-frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,10 @@
 export default function ChatContainer(){
     return (
         <>
+        <div className="chat-container">
          <h1 className="text-2xl font-bold">ChatContainer</h1>
          <p>Need to do chats-(container) here</p>
+         </div>
          
         </>
     )
