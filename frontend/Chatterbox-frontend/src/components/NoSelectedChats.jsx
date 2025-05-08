c345e254b6f653afcd459d810eb7d44c444b4310
Krrithik <krrithikezhil30@gmail.com>
2025-05-08 09:20:10 -0700

    starter code for when no chat is selected is set

--- frontend/Chatterbox-frontend/src/components/NoSelectedChats.jsx
+++ frontend/Chatterbox-frontend/src/components/NoSelectedChats.jsx
@@ -1,9 +1,17 @@
+import "../index.css"
+
 export default function NoSelectedChats(){
     return (
-        <>
-         <h1 className="text-2xl font-bold">nothing is selected yet</h1>
-         <p>select something</p>
-         
-        </>
-    )
+        <div className="nochat-root">
+          <div className="nochat-content">
+            
+    
+            {/* Welcome Text */}
+            <h2 className="nochat-title">Welcome to ChatApp!</h2>
+            <p className="nochat-desc">
+              Select a conversation from the sidebar to start chatting
+            </p>
+          </div>
+        </div>
+      );
 }
